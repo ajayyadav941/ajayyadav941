728f1942597b01d350afea5ad3eafee5bcfe0ef3
ajayyadav941 <71787356+ajayyadav941@users.noreply.github.com>
2022-04-09 11:05:33 +0530

    netconf 2nd script

--- xe-capa2.py
+++ xe-capa2.py
@@ -1,16 +1,26 @@
 from ncclient import manager
+from getpass import getpass
+from xml.dom.minidom import parseString
 
-xe = {
-    'host': '192.168.247.147',
-    'port':'830',
-    'username':'admin',
-    'password':'cisco',
-    'hostkey_verify':False
-}
+Username = input('Enter Your Username:')
+Password = input('Enter Your Password:')
+
+if Username == 'admin' and Password=='cisco':
+    xe = {
+        'host': '192.168.247.147',
+        'port':'830',
+        'username':Username,
+        'password':Password,
+        'hostkey_verify':False
+    }
 
 
-netconf = manager.connect(**xe)
-print('Netconf Connection EST with '+ xe['host'])
+    netconf = manager.connect(**xe)
+    print('#'*75+'\n'+ 'Netconf Connection EST with '+ xe['host'] + '\n'+ '#'*75)
+    running_config= netconf.get_config(source= 'running')
+    pretty_config = parseString(running_config.xml).toprettyxml()
+    print(pretty_config)
 
-for i in netconf.server_capabilities:
-    print(i)
+else:
+    print('Please Check Usernmae/Password')
+
